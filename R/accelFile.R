94304a14d1a436344f8485748753e277a3864394
agougher <agougherty@al.umces.edu>
2018-02-22 17:07:51 -0500

    Adding accelFile

---
+++ R/accelFile.R
@@ -0,0 +1,17 @@
+#' Raw accelerometer file
+#'
+#'
+#' @docType data
+#'
+#' @usage data(accelFile)
+#'
+#' @format
+#'
+#' @keywords datasets
+#'
+#' @references
+#'
+#' @source
+#'
+#' @examples
+"accelFile"
